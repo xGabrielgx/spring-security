efb41e72d803bbbf2e7ebdff038b7e6bdf179c71
Gabriel Xavier <gabrielgx928@gmail.com>
2025-04-24 14:12:05 -0300

    BCrypt password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$fCyvnzDnY0RHM6z3jejx9OJYi15SisVBSrPcuJU/agaLvXKuebuG2');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$fCyvnzDnY0RHM6z3jejx9OJYi15SisVBSrPcuJU/agaLvXKuebuG2');
 
 ----------------------------------- padrão spring security 'ROLE_ALGUMACOISA'
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
